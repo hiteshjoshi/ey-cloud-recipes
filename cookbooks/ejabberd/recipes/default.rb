7124ff1c6ddd25673a3e1f2b12460376cc8048c4
Hitesh Joshi <me@hiteshjoshi.com>
2013-02-19 13:50:22 +0530

    ejabberd

--- cookbooks/ejabberd/recipes/default.rb
+++ cookbooks/ejabberd/recipes/default.rb
@@ -1,11 +1,11 @@
-EJABBV = "2.1.9"
+EJABBV = "2.1.11"
 
 script "download Ejabberd" do
   not_if "test -d /tmp/ejabberd-#{EJABBV}"
   interpreter "bash"
   cwd "/tmp"
   code <<-SH
-  wget http://www.process-one.net/downloads/ejabberd/#{EJABBV}/ejabberd-#{EJABBV}.tar.gz
+  wget http://www.process-one.net/downloads/ejabberd/2.1.11/ejabberd-2.1.11.tgz
   tar xzf ejabberd-#{EJABBV}.tar.gz
   SH
 end
